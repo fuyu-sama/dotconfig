63518d4e4c49d028271861ae758ecdfc447fcd5b
fuyu-sama-admin1 <yingzhouhong@outlook.com>
2021-05-31 20:39:42 +0800

    Add update.sh for auto-updating

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+# update git repository
+git pull origin
+# ptpython
+ln -f ptpython/config.py $HOME/.config/ptpython/config.py
+# tmux
+ln -f tmux/tmux.conf $HOME/.tmux.conf
+# vim
+ln -f vim/vimrc $HOME/.vimrc
+ln -f vim/tasks.ini $HOME/.vim/tasks.ini
+[ -d $HOME/.vim/UltiSnips ] && rm $HOME/.vim/UltiSnips
+ln -f -s $DIR/vim/UltiSnips $HOME/.vim/UltiSnips
+# zsh
+ln -f zsh/zshrc $HOME/.zshrc
+ln -f zsh/myys.zsh-theme $HOME/.oh-my-zsh/custom/themes/myys.zsh-theme
