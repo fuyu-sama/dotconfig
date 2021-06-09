a20079c0fde43e0e2592243c5a0cbd2c5ccb705c
fuyu-sama <yingzhouhong@outlook.com>
2021-06-09 09:43:29 +0800

    Add install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,37 @@ dist==`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
 
 if [[ $dist == "debian" ]] || [[ $dist == "ubuntu" ]]
 then
-    apt update
-    apt install git zsh tmux make cmake neofetch
+    sudo apt update
+    sudo apt install zsh tmux make cmake neofetch
+    # python build environment
+    sudo apt install build-essential libssl-dev zlib1g-dev libbz2-dev \
+        libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev \
+        xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 elif [[ $dist == "opensuse-leap" ]] 
 then
-    zypper install git zsh tmux make cmake neofetch
+    sudo zypper install zsh tmux make cmake neofetch
+    # python build environment
+    sudo zypper install gcc automake bzip2 libbz2-devel xz xz-devel \
+        openssl-devel ncurses-devel readline-devel zlib-devel tk-devel \
+        libffi-devel sqlite3-devel
+
 elif [[ $dist == "arch" ]]
 then
-    pacman -S git zsh tmux make cmake neofetch
+    sudo pacman -S zsh tmux make cmake neofetch
+    # python build environment
+    sudo pacman -S --needed base-devel openssl zlib xz
 fi
+
+# vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# pyenv
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+
+# ohmyzsh
+sh zsh/install-ohmyzsh.sh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+./update.sh
