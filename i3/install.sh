77f2df26bf0663effd1f947747ca3c551c53d90d
fuyu-sama <yingzhouhong@outlook.com>
2021-10-08 15:04:14 +0800

    Use grayblocks theme for polybar && Use picom

--- i3/install.sh
+++ i3/install.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 sudo pacman -S \
     i3 mpd python-pywal calc pango feh rofi
-yay -S polybar i3lock-fancy-git 
+yay -S polybar i3lock-fancy-git picom-ibhagwan-git 
 
 ln -f $DIR/config $HOME/.config/i3
 
-[ -d $HOME/.config/polybar ] && rm $HOME/.config/polybar
+[ -d $HOME/.config/polybar ] && rm -rf $HOME/.config/polybar
 ln -f -s $DIR/polybar $HOME/.config/polybar
