de3674656386eb47d4f6d8ce702e6f46b1eb75f3
fuyu-sama <yingzhouhong@outlook.com>
2021-12-24 16:16:31 +0800

    Format

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ then
 fi
 dist=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
 
-if [[ $dist == "debian" ]] || [[ $dist == "ubuntu" ]]
-then
+if [[ $dist == "debian" ]] || [[ $dist == "ubuntu" ]]; then
     sudo apt update
     sudo apt install -y zsh tmux make cmake neofetch htop ncdu build-essential \
         libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget \
@@ -15,8 +14,7 @@ then
         libffi-dev liblzma-dev libncurses5-dev libgtk2.0-dev libatk1.0-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev ruby-dev lua5.1 \
         liblua5.1-dev libperl-dev
-elif [[ $dist == '"opensuse-leap"' ]]
-then
+elif [[ $dist == '"opensuse-leap"' ]]; then
     sudo zypper install -y zsh make cmake gcc-c++ gcc9 gcc9-c++ neofetch htop \
         ncdu automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel \
         readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel libevent-devel
@@ -28,13 +26,11 @@ then
     ./configure
     make && sudo make install
     cd -
-elif [[ $dist == '"opensuse-tumbleweed"' ]]
-then
+elif [[ $dist == '"opensuse-tumbleweed"' ]]; then
     sudo zypper install -y zsh make cmake neofetch tmux htop ncdu gcc gcc-c++ \
         automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel \
         readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel libevent-devel
-elif [[ $dist == "arch" ]]
-then
+elif [[ $dist == "arch" ]]; then
     sudo pacman -Sy --needed zsh tmux make cmake neofetch htop ncdu base-devel \
         openssl zlib xz nnn python-pip
 fi
@@ -64,20 +60,22 @@ cd Downloads/autojump
 cd -
 
 # vim
-export LDFLAGS="-rdynamic"
-git clone https://github.com/vim/vim.git Downloads/vim
-cd Downloads/vim
-./configure --with-features=huge \
-    --enable-multibyte \
-    --enable-rubyinterp=yes \
-    --enable-python3interp=yes \
-    --with-python-config-dir=$HOME/.pyenv/versions/3.8.10/lib/python3.8/config-3.8-x86_64-linux-gnu \
-    --enable-perlinterp=yes \
-    --enable-luainterp=yes \
-    --enable-cscope \
-    --prefix=$HOME/.local/prefix/vim/8.2
-make && make install
-cd -
+if ! [[ $dist == "arch" ]]; then
+    export LDFLAGS="-rdynamic"
+    git clone https://github.com/vim/vim.git Downloads/vim
+    cd Downloads/vim
+    ./configure --with-features=huge \
+        --enable-multibyte \
+        --enable-rubyinterp=yes \
+        --enable-python3interp=yes \
+        --with-python-config-dir=$HOME/.pyenv/versions/3.8.10/lib/python3.8/config-3.8-x86_64-linux-gnu \
+        --enable-perlinterp=yes \
+        --enable-luainterp=yes \
+        --enable-cscope \
+        --prefix=$HOME/.local/prefix/vim/8.2
+    make && make install
+    cd -
+fi
 
 git clone https://github.com/junegunn/vim-plug.git Downloads/vim-plug
 [ ! -d $HOME/.vim/autoload ] && mkdir $HOME/.vim/autoload
