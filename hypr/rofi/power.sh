821e4ef1780f9ceff1440cc713ead32648365881
fuyu-sama <yingzhouhong@outlook.com>
2024-01-11 16:29:51 +0800

    Add volume, power drawer and window rewrite

--- hypr/rofi/power.sh
+++ hypr/rofi/power.sh
@@ -4,24 +4,23 @@ prompt="rofi -dmenu -theme ~/.config/rofi/power.rasi"
 
 uptime=$(uptime -p | sed -e 's/up //g')
 
-shutdown=" Shutdown"
-reboot="󰑓 Restart"
-lock=" Lock"
-logout="󰍃 Logout"
-sleep="⏾ Sleep"
 cancel="󰜺 Cancel"
+sleep="⏾ Sleep"
+logout="󰍃 Logout"
+reboot="󰑓 Reboot"
+power=" Power"
 
-option="$cancel\n$sleep\n$shutdown\n$reboot\n$logout"
+option="$cancel\n$sleep\n$logout\n$reboot\n$power"
 
 select="$(echo -e "$option" | $prompt -p "Uptime - $uptime")"
 
 case $select in
-	$shutdown)
-		systemctl poweroff;;
-	$reboot)
-		systemctl reboot;;
 	$logout)
         hyprctl dispatch exit;;
     $sleep)
-        hyprctl dispatch dpms off
+        hyprctl dispatch dpms off;;
+	$reboot)
+		systemctl reboot;;
+	$power)
+		systemctl poweroff
 esac
