916728eceaa7e056a45278d76e5c6783e1d96fe0
fuyu-sama <yingzhouhong@outlook.com>
2024-07-16 11:16:38 +0800

    Add confirm menu

--- hypr/rofi/power.sh
+++ hypr/rofi/power.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-prompt="rofi -dmenu -theme ~/.config/rofi/power.rasi"
+prompt="rofi -dmenu -theme $HOME/.config/rofi/power.rasi"
+prompt_confirm="rofi -dmenu -theme $HOME/.config/rofi/confirm.rasi"
 
 uptime=$(uptime -p | sed -e 's/up //g')
 
@@ -12,15 +13,24 @@ power=" Power"
 
 option="$cancel\n$sleep\n$logout\n$reboot\n$power"
 
-select="$(echo -e "$option" | $prompt -p "Uptime - $uptime")"
+select=${1:-`echo -e "$option" | $prompt -p "Uptime - $uptime"`}
+
+function confirm() {
+    select="$(echo -e "󰸞 Yes\n No" | $prompt_confirm -p "$1?")"
+    if [[ $select == "󰸞 Yes" ]]; then
+        exec $2
+    else
+        bash $HOME/.config/rofi/power.sh
+    fi
+}
 
 case $select in
-	$logout)
-        hyprctl dispatch exit;;
+    $logout)
+        confirm "Logout" "hyprctl dispatch exit";;
     $sleep)
         hyprctl dispatch dpms off;;
-	$reboot)
-		systemctl reboot;;
-	$power)
-		systemctl poweroff
+    $reboot)
+        confirm "Reboot" "systemctl reboot";;
+    $power)
+        confirm "Power off" "systemctl poweroff"
 esac
