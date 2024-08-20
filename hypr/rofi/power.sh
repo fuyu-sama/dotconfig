47bc49924831bb4d22c65c8b19e894d586913d7b
fuyu-sama <yingzhouhong@outlook.com>
2024-08-20 09:43:14 +0800

    modify confirm

--- hypr/rofi/power.sh
+++ hypr/rofi/power.sh
@@ -14,14 +14,22 @@ power=" Power"
 
 option="$cancel\n$lock\n$logout\n$reboot\n$power"
 
-select=${1:-`echo -e "$option" | $prompt -p "Uptime - $uptime"`}
+if [[ -z $1 ]]; then
+    select=`echo -e "$option" | $prompt -p "Uptime - $uptime"`
+    env="rofi"
+else
+    select=$1
+    env="waybar"
+fi
 
 function confirm() {
     select="$(echo -e "󰸞 Yes\n No" | $prompt_confirm -p "$1?")"
     if [[ $select == "󰸞 Yes" ]]; then
         exec $2
     else
-        bash $HOME/.config/rofi/power.sh
+        if [[ $env == "rofi" ]]; then
+            bash $HOME/.config/rofi/power.sh
+        fi
     fi
 }
 
