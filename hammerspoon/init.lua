539e9a69f1466146aa06efb9274887c49de3767c
fuyu-sama <yingzhouhong@outlook.com>
2026-05-14 10:56:42 +0800

    hammerspoon little fix

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,6 +6,7 @@ local iterm2 = require("iterm2")
 local screenSwitcher = require("screenSwitcher")
 local inputSwitcher = require("inputSwitcher")
 -- local windowMover = require("windowMover")
+
 -- 启动所有模块
 spaceCtrl.init()
 iterm2.init()
